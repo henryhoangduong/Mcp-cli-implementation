8c21bee692d2b97d33dfc93c7368dcc451c3896c
henryhoangduong <henryhoangduong@gmail.com>
2025-03-20 22:20:22 +0700

    json rpc message

---
+++ messages/json_rpc_message.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Any, Dict, Optional
+
+
+class JSONRPCMessage(BaseModel):
+    jsonrpc: str = "2.0"
+    id: Optional[str] = None
+    params: Optional[Dict[str, Any]] = None
+    result: Optional[Dict[str, Any]] = None
+    error: Optional[Dict[str, Any]] = None
+
+    class Config:
+        extra = "allow"
+
+
+
---
+++ messages/send_initialize_message.py
@@ -0,0 +1,4 @@
+from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
+from pydantic import  BaseModel,Field
+from messages.json_rpc_message import JSONRPCMessage
+
